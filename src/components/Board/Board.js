752979fd420f99724c1f54d3db601ab9c1c94457
xujunhui <maxjunhuixu@gmail.com>
2018-08-31 14:58:43 +0800

    checking when user changes cell is done

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,76 +1,87 @@
-    import React, { Component } from 'react';
-    import Cell from "./Cell";
-    import './Board.css';
+import React, { Component } from 'react';
+import Cell from "./Cell";
+import './Board.css';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
-    export default class Board extends Component {
+//for alert in Cell when user inputs
+const options = {
+    position: 'bottom center',
+    timeout: 5000,
+    offset: '30px',
+    transition: 'scale'
+};
 
-        renderBoard = () => {
-            const { board, targetR, forKey } = this.props;
-            const checkExist = this.checkExist;
-            const renderCell = this.renderCell;
-            const rowsView = [];
-            board.map((row, index) => {
-                const rowView = [];
-                let exist = false;
-                row.map((cell, jndex) => {
-                    exist = checkExist(targetR, index, jndex);
-                    rowView.push(renderCell(exist, cell));
-                    return rowView;
-                });
+export default class Board extends Component {
 
-                rowsView.push(
-                    <div className="row" key={"row-" + forKey + index}>
-                        {rowView}
-                    </div>
-                );
-                return rowsView;
-            });
-            return (
-                <div>
-                    {rowsView}
-                </div>
-            )
-        };
-
-        checkExist = (arr, index, jndex) => {
+    renderBoard = () => {
+        const { board, targetR, forKey } = this.props;
+        const checkExist = this.checkExist;
+        const renderCell = this.renderCell;
+        const rowsView = [];
+        board.map((row, index) => {
+            const rowView = [];
             let exist = false;
-            arr[0].map((item) => {
-                return (item[1] === index && item[2] === jndex) ? exist = true : null;
+            row.map((cell, jndex) => {
+                exist = checkExist(targetR, index, jndex);
+                rowView.push(renderCell(exist, cell));
+                return rowView;
             });
-            return exist;
-        };
 
-        renderCell = (exist, cell) => {
-            const { forKey } = this.props;
-            const forClass = "shouldColor";
+            rowsView.push(
+                <div className="row" key={"row-" + forKey + index}>
+                    {rowView}
+                </div>
+            );
+            return rowsView;
+        });
+        return (
+            <div>
+                {rowsView}
+            </div>
+        )
+    };
+
+    checkExist = (arr, index, jndex) => {
+        let exist = false;
+        arr[0].map((item) => {
+            return (item[1] === index && item[2] === jndex) ? exist = true : null;
+        });
+        return exist;
+    };
+
+    renderCell = (exist, cell) => {
+        const { forKey } = this.props;
+        const forClass = "shouldColor";
+        return (
+            <div>
+                { exist ? (
+                    <Cell cell={cell}
+                          forClassName={forClass}
+                          key= {"cell-"+ forKey + cell.rowId.toString() + cell.columnId.toString()}
+                    /> ) : (
+                    <Cell cell={cell}
+                          key= {"cell-"+ forKey + cell.rowId.toString() + cell.columnId.toString()}
+                    />
+                )}
+            </div>
+        )
+    };
+
+    render() {
+        const { targetR, board, cellChange } = this.props;
+        if (targetR !== undefined) {
+            const boardView = this.renderBoard();
             return (
-                <div>
-                    { exist ? (
-                        <Cell cell={cell}
-                              forClassName={forClass}
-                              key= {"cell-"+ forKey + cell.rowId.toString() + cell.columnId.toString()}
-                        /> ) : (
-                        <Cell cell={cell}
-                              key= {"cell-"+ forKey + cell.rowId.toString() + cell.columnId.toString()}
-                        />
-                    )}
+                <div className="container">
+                    <div className="board">
+                        {boardView}
+                    </div>
                 </div>
             )
-        };
-
-        render() {
-            const { targetR, board, cellChange } = this.props;
-            if (targetR !== undefined) {
-                const boardView = this.renderBoard();
-                return (
-                    <div className="container">
-                        <div className="board">
-                            {boardView}
-                        </div>
-                    </div>
-                )
-            } else {
-                return (
+        } else {
+            return (
+                <AlertProvider template={AlertTemplate} {...options}>
                     <div className="container">
                         <div className="board">
                             {board.map((row, index) => {
@@ -89,9 +100,10 @@
                             })}
                         </div>
                     </div>
-                );
-
-            }
+                </AlertProvider>
+            );
 
         }
+
     }
+}
--- src/components/Board/Cell.js
+++ src/components/Board/Cell.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import "./Cell.css";
 import CellData from "../../data/CellData";
+import { withAlert } from 'react-alert';
 
 class Cell extends Component {
 
     //handle input change
     updateCell(e) {
         const {cell, changeBoardFromCell} = this.props;
-        const userInput = e.target.value;
-        const newCell = new CellData(userInput, cell.rowId, cell.columnId);
+        const userInput = e.target.value.toLowerCase();
+        let uInput = userInput;
+        //alert when user keys in unconditional stuff
+        if (userInput.length > 0) {
+            if (userInput.length > 1) {
+                this.props.alert.show("One letter, please.");
+                uInput = userInput.charAt(0);
+            }
+
+            let charCodeOfInput = userInput.charCodeAt(0);
+            if (charCodeOfInput !== 42
+                && !(charCodeOfInput >= 65 && charCodeOfInput <= 90)
+                && !(charCodeOfInput >= 97 && charCodeOfInput <= 122)
+                && !(isNaN(charCodeOfInput))) {
+                this.props.alert.show("Letter, please.");
+                uInput = cell.letter;
+            }
+        }
+
+        const newCell = new CellData(uInput, cell.rowId, cell.columnId);
         changeBoardFromCell(newCell);
     }
 
@@ -39,4 +58,4 @@ class Cell extends Component {
     }
 }
 
-export default Cell;
+export default withAlert(Cell);
